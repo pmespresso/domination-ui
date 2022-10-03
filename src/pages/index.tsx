6e58b9079ae3ce47389a1f0746c5bd3ef9461d4c
pandl <yjkimjunior@gmail.com>
2022-10-03 11:59:39 +0800

    setup wagmi, tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { configureChains, chain, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const Home: NextPage = () => {
+  const { chains, provider, webSocketProvider } = configureChains(
+    [chain.mainnet, chain.polygon],
+    [publicProvider()]
+  );
+
+  const client = createClient({
+    autoConnect: true,
+    provider,
+    webSocketProvider,
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Domination</title>
+        <meta
+          name="description"
+          content="Simple, on-chain winner takes all board game."
+        />
+      </Head>
+      <WagmiConfig client={client}>
+        <main className="container bg-gradient-to-r from-cyan-500 to-blue-500 mx-auto bg-cover bg-center h-px w-px">
+          {chain.arbitrum.id}
+        </main>
+      </WagmiConfig>
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        backgroundImage: "url('/images/background.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
